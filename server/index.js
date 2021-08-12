f4f13a270a68f8ee4d7aef97e5e9559feea922e3
Gabe Wilk <gabe.wilk@gmail.com>
2021-08-12 09:20:48 -0700

    new

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+const passport = require('passport');
+mongoose.connect(keys.mongoURI);
+require('./models/User');
 require('./services/passport');
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 100,
+    keys: [keys.cookieKey],
+  })
+);
+//make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRoutes')(app);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,6 +5,15 @@ module.exports = (app) => {
     '/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
   );
-
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/current_user', (req, res) => {
+    // res.send(req.session);
+    res.send(req.user);
+  });
 };
